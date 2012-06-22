a34e4d6a7bf9b747cffd7513f6a62c142b71778f
Luna-Rawas <luna.rawas@live.com>
2012-06-22 20:09:49 +0300

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  get "static_pages/home"
 
-  get "pages/contact"
+  get "static_pages/help"
+  
+  get "static_pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
